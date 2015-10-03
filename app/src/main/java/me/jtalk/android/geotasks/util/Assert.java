aa2a3f84ae070f42d1cd1986ed078983ac6213e2
Jtalk <me@jtalk.me>
2015-10-03 22:36:34 +0300

    An IllegalArgumentException wrapper.

---
+++ app/src/main/java/me/jtalk/android/geotasks/util/Assert.java
@@ -0,0 +1,21 @@
+package me.jtalk.android.geotasks.util;
+
+import java.text.MessageFormat;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Assert {
+
+	public static void verifyArgument(boolean expression, String message) {
+		if (!expression) {
+			throw new IllegalArgumentException(message);
+		}
+	}	public static void verifyArgument(boolean expression, String format, Object ...args) {
+		if (!expression) {
+			String message = MessageFormat.format(format, args);
+			throw new IllegalArgumentException(message);
+		}
+	}
+}
