1710d48b1908257bb47fcc090ff371222b94f68c
liza <liza@localhost>
2015-10-27 21:58:26 +0300

    Latitude must be displayed first.

--- app/src/main/java/me/jtalk/android/geotasks/util/GeoPointFormat.java
+++ app/src/main/java/me/jtalk/android/geotasks/util/GeoPointFormat.java
@@ -6,22 +6,22 @@ import org.osmdroid.util.GeoPoint;
 import java.text.MessageFormat;
 
 public class GeoPointFormat {
-	private static final String FORMAT_GEOPINT = "lon:{0} lat:{1}";
+	private static final String FORMAT_GEOPINT = "lat: {0} lon: {1}";
 	private static final String FORMAT_GEOPINT_SIMPLE = "{0} {1}";
 
 	public static String format(IGeoPoint geoPoint) {
-		return MessageFormat.format(FORMAT_GEOPINT, geoPoint.getLongitude(), geoPoint.getLatitude());
+		return MessageFormat.format(FORMAT_GEOPINT, geoPoint.getLatitude(), geoPoint.getLongitude());
 	}
 
 	public static String formatSimple(IGeoPoint geoPoint) {
-		return MessageFormat.format(FORMAT_GEOPINT_SIMPLE, geoPoint.getLongitude(), geoPoint.getLatitude());
+		return MessageFormat.format(FORMAT_GEOPINT_SIMPLE, geoPoint.getLatitude(), geoPoint.getLongitude());
 	}
 
 	public static IGeoPoint parse(String str) {
 		String[] values = str.split(" ");
 
-		double lon = Double.valueOf(values[0]);
-		double lat = Double.valueOf(values[1]);
+		double lat = Double.valueOf(values[2]);
+		double lon = Double.valueOf(values[1]);
 
 		return new GeoPoint(lat, lon);
 	}
