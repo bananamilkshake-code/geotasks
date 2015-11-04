5f97e003f6a67f6ffa78b5257ab4d21657e3caec
liza <liza@localhost>
2015-11-04 21:41:57 +0300

    Create tests for Notifier and EventsLocationListener.

---
+++ app/src/androidTest/java/me/jtalk/android/geotasks/me/jtalk/android/application/NotifierTest.java
@@ -0,0 +1,20 @@
+package me.jtalk.android.geotasks.me.jtalk.android.application;
+
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.osmdroid.util.GeoPoint;
+
+import me.jtalk.android.geotasks.application.Notifier;
+import me.jtalk.android.geotasks.source.Event;
+
+public class NotifierTest extends AndroidTestCase {
+	@MediumTest
+	public void testCreateNotification() {
+		Notifier notifier = new Notifier(getContext());
+
+		Event event = new Event(0, "TestEvent", "2015/11/03 10:57AM", new GeoPoint(23.34, 34.32));
+
+		notifier.onEventIsNear(event);
+	}
+}
---
+++ app/src/androidTest/java/me/jtalk/android/geotasks/me/jtalk/android/application/listener/EventsLocationListenerTest.java
@@ -0,0 +1,64 @@
+package me.jtalk.android.geotasks.me.jtalk.android.application.listener;
+
+import android.location.Location;
+import android.test.InstrumentationTestCase;
+
+import org.mockito.Mockito;
+import org.osmdroid.util.GeoPoint;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import me.jtalk.android.geotasks.application.Notifier;
+import me.jtalk.android.geotasks.application.listeners.EventsLocationListener;
+import me.jtalk.android.geotasks.source.Event;
+import me.jtalk.android.geotasks.source.EventsSource;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+public class EventsLocationListenerTest extends InstrumentationTestCase {
+	@Override
+	public void setUp() throws Exception {
+		System.setProperty("dexmaker.dexcache", getInstrumentation().getTargetContext().getCacheDir().getPath());
+	}
+
+	public void testNearLocationChoose() {
+		Event event1 = new Event(1, "Event1", "Time1", new GeoPoint(0.0, 0.0));
+		Event event2 = new Event(2, "Event2", "Time2", new GeoPoint(55.63947, 27.03267));
+		Event event3 = new Event(3, "Event3", "Time3", new GeoPoint(55.63847, 27.03257));
+
+		List<Event> activeEvents = new ArrayList<>();
+		activeEvents.add(event1);
+		activeEvents.add(event2);
+		activeEvents.add(event3);
+
+		EventsSource mockEventsSource = Mockito.mock(EventsSource.class);
+		when(mockEventsSource.getActive(any(Calendar.class))).thenReturn(activeEvents);
+
+		Notifier mockNotifier = Mockito.mock(Notifier.class);
+		doNothing().when(mockNotifier).onEventIsNear(any());
+
+		EventsLocationListener eventLocationListener = new EventsLocationListener();
+		eventLocationListener.setDistanceToAlarm(100);
+		eventLocationListener.setEventsSource(mockEventsSource);
+		eventLocationListener.setNotifier(mockNotifier);
+
+		Location mockCurrentLocation = Mockito.mock(Location.class);
+		when(mockCurrentLocation.getLatitude()).thenReturn(55.63914);
+		when(mockCurrentLocation.getLongitude()).thenReturn(27.03195);
+
+		eventLocationListener.onLocationChanged(mockCurrentLocation);
+
+		verify(mockEventsSource).getActive(any(Calendar.class));
+		verifyNoMoreInteractions(mockEventsSource);
+
+		verify(mockNotifier).onEventIsNear(event2);
+		verify(mockNotifier).onEventIsNear(event3);
+		verifyNoMoreInteractions(mockNotifier);
+	}
+}
