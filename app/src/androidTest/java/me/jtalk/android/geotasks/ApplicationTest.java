cddebd89bda7217049df7aed0c5e196b3b20cd42
liza <liza@localhost>
2015-11-04 22:28:31 +0300

    Not needed.

--- app/src/androidTest/java/me/jtalk/android/geotasks/ApplicationTest.java
+++ app/src/androidTest/java/me/jtalk/android/geotasks/ApplicationTest.java
@@ -5,9 +5,6 @@ import android.test.ApplicationTestCase;
 
 import me.jtalk.android.geotasks.application.GeoTasksApplication;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest extends ApplicationTestCase<GeoTasksApplication> {
 	public ApplicationTest() {
 		super(GeoTasksApplication.class);
