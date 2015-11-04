dbf8dfa618053c3cef9ac6154ee07a6d41695c4c
liza <liza@localhost>
2015-11-04 22:21:47 +0300

    GeoTaskApplication is main application class.

--- app/src/androidTest/java/me/jtalk/android/geotasks/ApplicationTest.java
+++ app/src/androidTest/java/me/jtalk/android/geotasks/ApplicationTest.java
@@ -3,11 +3,13 @@ package me.jtalk.android.geotasks;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import me.jtalk.android.geotasks.application.GeoTasksApplication;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ApplicationTestCase<GeoTasksApplication> {
 	public ApplicationTest() {
-		super(Application.class);
+		super(GeoTasksApplication.class);
 	}
 }
