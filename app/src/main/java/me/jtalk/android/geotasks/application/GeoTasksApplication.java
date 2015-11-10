51137ec28c469d65e5d3210ceb679e6a034808f8
liza <liza@localhost>
2015-11-11 00:25:16 +0300

    Initialize ACRA on application startup. When crash happens notification will be sent to emal via acracadabra (http://livefront.github.io/acracadabra).

--- app/src/main/java/me/jtalk/android/geotasks/application/GeoTasksApplication.java
+++ app/src/main/java/me/jtalk/android/geotasks/application/GeoTasksApplication.java
@@ -2,13 +2,28 @@ package me.jtalk.android.geotasks.application;
 
 import android.app.Application;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
+import me.jtalk.android.geotasks.BuildConfig;
 import me.jtalk.android.geotasks.source.EventsSource;
 import me.jtalk.android.geotasks.util.Logger;
 
+@ReportsCrashes(
+		formUri = "http://192.168.1.127:5000/reports",
+		buildConfigClass = BuildConfig.class
+)
 public class GeoTasksApplication extends Application {
 	private static final Logger LOG = new Logger(GeoTasksApplication.class);
 
 	private EventsSource eventsSource;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		ACRA.init(this);
+	}
 
 	public EventsSource getEventsSource() {
 		return eventsSource;
