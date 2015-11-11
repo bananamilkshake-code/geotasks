ec2e1ddff79bc4f74073f6753841e44dda0efd94
liza <liza@localhost>
2015-11-11 22:09:13 +0300

    ACRA will ask user if he wants to send crash report.

--- app/src/main/java/me/jtalk/android/geotasks/application/GeoTasksApplication.java
+++ app/src/main/java/me/jtalk/android/geotasks/application/GeoTasksApplication.java
@@ -3,15 +3,22 @@ package me.jtalk.android.geotasks.application;
 import android.app.Application;
 
 import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 import me.jtalk.android.geotasks.BuildConfig;
+import me.jtalk.android.geotasks.R;
 import me.jtalk.android.geotasks.source.EventsSource;
 import me.jtalk.android.geotasks.util.Logger;
 
 @ReportsCrashes(
 		formUri = "http://192.168.1.127:5000/reports",
-		buildConfigClass = BuildConfig.class
+		buildConfigClass = BuildConfig.class,
+		mode = ReportingInteractionMode.DIALOG,
+		resToastText = R.string.crash_toast_text,
+		resDialogTitle = R.string.crash_dialog_title,
+		resDialogText = R.string.crash_dialog_text,
+		resDialogOkToast = R.string.crash_dialog_ok_toast
 )
 public class GeoTasksApplication extends Application {
 	private static final Logger LOG = new Logger(GeoTasksApplication.class);
