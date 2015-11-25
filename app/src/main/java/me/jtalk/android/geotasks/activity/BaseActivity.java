cf4d24de91e020749f8079162ef0c620de4949ea
liza <liza@localhost>
2015-11-25 20:31:03 +0300

    Not needed.

--- app/src/main/java/me/jtalk/android/geotasks/activity/BaseActivity.java
+++ app/src/main/java/me/jtalk/android/geotasks/activity/BaseActivity.java
@@ -4,9 +4,7 @@ import android.app.Activity;
 import android.os.Build;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import lombok.NonNull;
 import me.jtalk.android.geotasks.application.GeoTasksApplication;
