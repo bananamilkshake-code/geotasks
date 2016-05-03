ba4a935a1482a3df364f1b42d8ceda2cd3d5f9b2
bananamilkshake <bananamilkshake@localhost>
2016-05-03 18:59:21 +0300

    Use integers to keep Acra enabling preference in application.

--- app/src/main/java/me/jtalk/android/geotasks/application/listeners/CrashReportsStatusChangeListener.java
+++ app/src/main/java/me/jtalk/android/geotasks/application/listeners/CrashReportsStatusChangeListener.java
@@ -21,7 +21,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
@@ -41,29 +43,26 @@ import me.jtalk.android.geotasks.util.Logger;
 public class CrashReportsStatusChangeListener implements SharedPreferences.OnSharedPreferenceChangeListener {
 	private static final Logger LOG = new Logger(CrashReportsStatusChangeListener.class);
 
-	private static final Map<String, Map<String, Boolean>> ACRA_PREFERENCES;
+	private static final List<Map<String, Boolean>> ACRA_PREFERENCES;
 
 	private static final String PREF_ACRA_DISABLED = "acra.disable";
 	private static final String PREF_ACRA_ALWAYSACCEPT = "acra.alwaysaccept";
 
-	private static final String STATUS_ASK = "Ask";
-	private static final String STATUS_ENABLE = "Enable";
-	private static final String STATUS_DISABLE = "Disable";
+	private static final int DEFAULT_STATUS = 0;
+	private static final String DEFAULT_STATUS_STR = String.valueOf(DEFAULT_STATUS);
 
-	private static final String DEFAULT_STATUS = STATUS_ASK;
-
-	private static void addPrefMapping(String prefValue, boolean isDisabled, boolean isAlwaysAccept) {
+	private static void addPrefMapping(boolean isDisabled, boolean isAlwaysAccept) {
 		Map<String, Boolean> prefMapping = new HashMap<>();
 		prefMapping.put(PREF_ACRA_DISABLED, isDisabled);
 		prefMapping.put(PREF_ACRA_ALWAYSACCEPT, isAlwaysAccept);
-		ACRA_PREFERENCES.put(prefValue, prefMapping);
+		ACRA_PREFERENCES.add(prefMapping);
 	}
 
 	static {
-		ACRA_PREFERENCES = new HashMap<>();
-		addPrefMapping(STATUS_ASK, false, false);
-		addPrefMapping(STATUS_ENABLE, false, true);
-		addPrefMapping(STATUS_DISABLE, true, true);
+		ACRA_PREFERENCES = new ArrayList<>();
+		addPrefMapping(false, false);
+		addPrefMapping(false, true);
+		addPrefMapping(true, true);
 	}
 
 	@NonNull
@@ -71,21 +70,19 @@ public class CrashReportsStatusChangeListener implements SharedPreferences.OnSha
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		if (key != context.getString(R.string.pref_crash_report_send_status)) {
+		if (!key.equals(context.getString(R.string.pref_crash_report_send_status))) {
 			return;
 		}
 
-		String newStatus = sharedPreferences.getString(key, DEFAULT_STATUS);
+		String newStatus = sharedPreferences.getString(key, DEFAULT_STATUS_STR);
 
 		LOG.debug("Crash report status changed to {0}", newStatus);
 
-		Map<String, Boolean> mappedPrefs = ACRA_PREFERENCES.get(newStatus);
-
+		Integer statusKey = Integer.getInteger(newStatus, DEFAULT_STATUS);
 		SharedPreferences.Editor editor = sharedPreferences.edit();
-		for (Map.Entry<String, Boolean> entry : mappedPrefs.entrySet()) {
+		for (Map.Entry<String, Boolean> entry : ACRA_PREFERENCES.get(statusKey).entrySet()) {
 			editor.putBoolean(entry.getKey(), entry.getValue());
 		}
-
-		editor.commit();
+		editor.apply();
 	}
 }
