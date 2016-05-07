53f992b65ccc3ecc00d940320c6fa908699ee9ec
bananamilkshake <bananamilkshake@localhost>
2016-05-07 23:29:06 +0300

    This methods not needed.

--- app/src/main/java/me/jtalk/android/geotasks/application/listeners/MapGestureDetector.java
+++ app/src/main/java/me/jtalk/android/geotasks/application/listeners/MapGestureDetector.java
@@ -30,7 +30,7 @@ import me.jtalk.android.geotasks.location.TaskCoordinates;
 /**
  * Mark on the map must be placed if only single tap has occurred. OnTouchEvent doesn't provide
  * convenient way to detect if touch was single, that's why implementation of GestureListener is
- * needed: GestureDetector can identify click type more precisely and single tap can be catched
+ * needed: GestureDetector can identify click type more precisely and single tap can be caught
  * easily.
  */
 @AllArgsConstructor
@@ -42,15 +42,5 @@ public class MapGestureDetector extends GestureDetector.SimpleOnGestureListener 
 		LatLong pickedLatLong = new MapViewProjection(locationPickActivity.getMapView()).fromPixels(event.getX(), event.getY());
 		locationPickActivity.onLocationPick(new TaskCoordinates(pickedLatLong));
 		return true;
-	}
-
-	@Override
-	public boolean onScroll(MotionEvent event1, MotionEvent event2, final float distanceX, float distanceY) {
-		return super.onScroll(event1, event2, distanceX, distanceY);
-	}
-
-	@Override
-	public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
-		return super.onFling(event1, event2, velocityX, velocityY);
 	}
 }
